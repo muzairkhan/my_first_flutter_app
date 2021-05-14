f589565359b8ebd23c36f00fb6b4a6b628d6a08a
Muhammad Uzair Khan <m.uzairkhan799@gmail.com>
2021-05-14 21:47:31 +0500

    work Accomplish

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Uzair Khan m.uzairkhan799@gmail.com'),
+      ),
+    ),
+  ));
+}
+
+
+
